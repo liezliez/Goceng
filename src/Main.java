9ffa89ffef42e2a9fa6c0e669a541ea5de196cdb
liezardaf <liezarda.f@gmail.com>
2025-03-20 14:37:52 +0700

    Commit kedua

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 import models.account.User;
 
-public static void main(String[] args) {
-    User user = new User(1L, "John Doe", "john.doe@example.com", "123456789", "password123");
+public class Main {
+    public static void main(String[] args) {
+
+        // Buat objek menggunakan constructor
+        User user = new User(1L, "John Doe", "john.doe@example.com", "123456789", "password123");
+
+        // Show informasi user
+        System.out.println("ID: " + user.getId());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Email: " + user.getEmail());
+        System.out.println("NIP: " + user.getNip());
+        System.out.println("Password: " + user.getPassword());
+    }
 }
