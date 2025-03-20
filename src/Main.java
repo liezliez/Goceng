57c010bbf9e42bfd70e1203500ec713056461561
liezardaf <liezarda.f@gmail.com>
2025-03-20 14:30:05 +0700

    Commit kedua

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
+import models.account.User;
+
+public static void main(String[] args) {
+    User user = new User(1L, "John Doe", "john.doe@example.com", "123456789", "password123");
 }
--- src/models/account/UserCustomer.java
+++ src/models/account/UserCustomer.java
@@ -1,4 +1,4 @@
-package models;
+package models.account;
 
 public class UserCustomer {
 }
