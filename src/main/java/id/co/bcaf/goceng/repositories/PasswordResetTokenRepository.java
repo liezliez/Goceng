89bd258ef118967f034b2ac0a9b2720660498f12
liezliez73 <liezliez73@gmail.com>
2025-05-27 23:16:03 +0700

    password reset fix

--- src/main/java/id/co/bcaf/goceng/repositories/PasswordResetTokenRepository.java
+++ src/main/java/id/co/bcaf/goceng/repositories/PasswordResetTokenRepository.java
@@ -3,6 +3,9 @@ package id.co.bcaf.goceng.repositories;
 import id.co.bcaf.goceng.models.PasswordResetToken;
 import id.co.bcaf.goceng.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,5 +13,8 @@ import java.util.UUID;
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, UUID> {
     Optional<PasswordResetToken> findByToken(String token);
     Optional<PasswordResetToken> findByUser(User user);
-    void deleteByUser(User user);
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM PasswordResetToken t WHERE t.user = :user")
+    void deleteByUser(@Param("user") User user);
+
 }
